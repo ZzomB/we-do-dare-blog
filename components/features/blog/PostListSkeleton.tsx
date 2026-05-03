78c502e79314043a72f1e7fbe074e49e0300f98f
ZzomB <mrjbk77@gmail.com>
2026-05-03 16:25:36 +0900

    skeleton layout modified

--- components/features/blog/PostListSkeleton.tsx
+++ components/features/blog/PostListSkeleton.tsx
@@ -1,10 +1,10 @@
 import { Skeleton } from '@/components/ui/skeleton';
 export default function PostListSkeleton() {
   return (
-    <div className="grid grid-cols-1 gap-4">
-      {Array.from({ length: 4 }).map((_, i) => (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      {Array.from({ length: 9 }).map((_, i) => (
         <div key={i} className="flex flex-col space-y-3">
-          <Skeleton className="h-[200px] w-full rounded-xl" />
+          <Skeleton className="aspect-video w-full rounded-xl" />
           <div className="space-y-2">
             <Skeleton className="h-4 w-16" />
             <Skeleton className="h-6 w-full" />
